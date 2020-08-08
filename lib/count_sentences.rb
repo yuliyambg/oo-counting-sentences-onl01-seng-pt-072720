966fa3553c63d2d711d46d8aaed642349142d1e6
Yuliya Marinova <yuliyambg@gmail.com>
2020-08-08 18:16:11 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+     return true
+    else
+     false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else
+      false
+    end
   end
 
   def count_sentences
-
-  end
+    self.split(/\.|\?|\!|\!!|\.../).delete_if { |word| word.size < 2}.size
+   end
+ 
+ 
 end
